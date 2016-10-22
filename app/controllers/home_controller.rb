ae492cf6e9711be4cd31fd0eddc4dddc6ad94a17
Abdusamed Sherif <abdusameds@gmail.com>
2016-10-22 21:42:04 +0300

    Specified Home Path

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,4 @@
+class HomeController < ApplicationController
+  def index
+  	@quote = Quote.get_random_quote
+end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,6 @@
 class Quote < ActiveRecord::Base
+	def self.get_random_quote
+  		order(“RANDOM()”).first
+	end
+	validates_presence_of :body
 end
